c398cf6521ab06493372a470c3ef823e94526d39
Willian Marioti <willian.marioti@gmail.com>
2023-05-05 20:03:45 -0300

    Criação das controllers da API principal.

---
+++ OnTheFly/Controllers/AirportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnTheFly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirportsController : ControllerBase
+    {
+    }
+}
