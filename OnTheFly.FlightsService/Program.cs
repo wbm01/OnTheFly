3f855864dc26064c9736ff4a33d88f2c3b85fe7c
matheus7k <06matheus.oliveira@gmail.com>
2023-05-07 11:51:24 -0300

    Criado microsserviço do mongo

--- OnTheFly.FlightsService/Program.cs
+++ OnTheFly.FlightsService/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.Extensions.Options;
+using OnTheFly.FlightsService.config;
+using OnTheFly.FlightsService.Repositories;
+using OnTheFly.FlightsService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection("MongoDBConfig"));
+
+builder.Services.AddSingleton<IMongoDBConfig>(s => s.GetRequiredService<IOptions<MongoDBConfig>>().Value);
+builder.Services.AddSingleton<FlightsRepository>();
+builder.Services.AddSingleton<FlightService>();
 
 var app = builder.Build();
 
