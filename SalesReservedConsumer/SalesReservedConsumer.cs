61ab0023ad492b6e9346151d4cc1469208c1770c
matheus7k <06matheus.oliveira@gmail.com>
2023-05-09 14:46:05 -0300

    Criado consumer para a fila Sold e Reserved

---
+++ SalesReservedConsumer/SalesReservedConsumer.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+HttpClient _client = new();
+const string QUEUE_NAME = "reserved";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(
+            queue: QUEUE_NAME,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        while (true)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var returnCustomer = Encoding.UTF8.GetString(body);
+                var customer = JsonConvert.DeserializeObject<Sale>(returnCustomer);
+            };
+
+            channel.BasicConsume(
+                queue: QUEUE_NAME,
+                autoAck: true,
+                consumer: consumer
+            );
+
+            Thread.Sleep(2000);
+        }
+    }
+}
